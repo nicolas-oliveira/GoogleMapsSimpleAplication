ca0d50e431c5332c669c062e88b9fb7f015eaa90
Nicolas Oliveira <nicolasm15@hotmail.com>
2022-03-12 02:35:40 -0300

    feat: adicionando geolocalização e integração com o mapa

--- app/src/main/java/com/progiv/googlemapssimpleaplication/MapsActivity.java
+++ app/src/main/java/com/progiv/googlemapssimpleaplication/MapsActivity.java
@@ -1,21 +1,45 @@
 package com.progiv.googlemapssimpleaplication;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.progiv.googlemapssimpleaplication.databinding.ActivityMapsBinding;
+import android.content.Context;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+    public Button btLocation;
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +54,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        // List<point> allpoints = new ArrayList<Obj>();
+        btLocation = (Button) findViewById(R.id.bt_location);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        btLocation.setOnClickListener(v -> {
+            if(ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        Location location = task.getResult();
+
+                        if(location != null) {
+                            LatLng youAreHere = new LatLng(location.getLatitude(),location.getLongitude());
+
+                            mMap.addMarker(new MarkerOptions().position(youAreHere).title("Você está aqui"));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(youAreHere));
+                        }
+                    }
+                });
+            } else {
+                ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+            }
+        });
         mMap = googleMap;
+
         // Add an event to listen user input and create a new marker
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
@@ -53,8 +91,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
         // Add a marker in Brazil and move the camera
-        LatLng brazil = new LatLng(-15.7213868,-48.0777876);
-        mMap.addMarker(new MarkerOptions().position(brazil).title("Marker in Brazil"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(brazil));
+        // LatLng brazil = new LatLng(-15.7213868,-48.0777876);
+        // mMap.addMarker(new MarkerOptions().position(brazil).title("Marker in Brazil"));
+        // mMap.moveCamera(CameraUpdateFactory.newLatLng(brazil));
     }
 }
